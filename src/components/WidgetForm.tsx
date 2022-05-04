37009909c379957cb2b8c62a60eb2a5991840faa
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-04 09:41:02 -0300

    :wrench: Finalizando Header e Footer

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,17 @@
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World!
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        
+      </header>
+
+      <p>Conteúdo</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+      </footer>
     </div>
   );
 }
