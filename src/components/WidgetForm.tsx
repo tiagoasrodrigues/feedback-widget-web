4365fc38650307ddebff534bd37c098cd62582fc
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-04 10:06:40 -0300

    :art: Adicionando icones no projeto

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outros',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento',
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -9,7 +37,9 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>Conteúdo</p>
+      <div className="flex py-8 gap-2 w-full">
+
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
