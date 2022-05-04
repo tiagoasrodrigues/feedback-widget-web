b30773432d7a5c83c9d6fb6126bb7f425f0e5199
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-04 09:50:40 -0300

    :wrench: Componente CloseButton

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,10 +1,12 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
 
-        
+        <CloseButton />
       </header>
 
       <p>Conteúdo</p>
