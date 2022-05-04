a4ec13f8db15d81b596c86f50a184ef44f3d965b
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-04 10:49:22 -0300

    :wrench: Criando feedbackTypes nos buttons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from '../assets/bug.svg'
@@ -28,7 +30,11 @@ const feedbackTypes = {
   },
 };
 
+type FeedbackType = keyof typeof feedbackTypes
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -37,9 +43,25 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-
+     {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+        { Object.entries(feedbackTypes).map(([key, value]) =>{         
+          return(
+            <button 
+            key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => setFeedbackType(key as FeedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        }) }
       </div>
+     ) : (
+       <p>Hello World!</p>
+     )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
